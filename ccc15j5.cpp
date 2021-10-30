74a9f69d10d4a113fc9da567de10c058aa00b267
Dundee <60833894+dundee-zhang@users.noreply.github.com>
2021-10-30 14:50:57 -0400

    Create ccc15j5.cpp

---
+++ ccc15j5.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+
+int solving(int piNum, int people, int minimum)
+{
+    // if the number of pie * people is the same as pi, only 1 way to give out the pie
+    if(minimum * people == piNum)
+    {
+        // return the answer 1
+        return 1;
+    }
+
+    // if there is 1 person, there is only one way to get the pi
+    if(people == 1)
+    {
+        // return the answer 1
+        return 1;
+    }
+  
+    int sum = 0;
+    for(int i = minimum; piNum / people >= i; i++)
+    {
+      sum += solving(piNum - i, people - 1, i);
+    }
+    return sum;
+}
+
+int main()
+{
+    int pi, people;
+    scanf("%i%i", &pi, &people);
+  
+    printf("%i", solving(pi, people, 1));
+
+    return 0;
+}
