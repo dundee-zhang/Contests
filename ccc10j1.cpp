91fe4a1997a89bc645a3eefd8874e3228e68edd0
Dundee <60833894+dundee-zhang@users.noreply.github.com>
2021-10-30 14:50:30 -0400

    Create ccc10j1.cpp

---
+++ ccc10j1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int x;
+    scanf("%i", &x);
+
+    switch(x)
+    {
+    case 1:
+        std::cout << 1;
+        break;
+    case 2:
+        std::cout << 2;
+        break;
+
+    case 3:
+        std::cout << 2;
+        break;
+
+    case 4:
+        std::cout << 3;
+        break;
+
+    case 5:
+        std::cout << 3;
+        break;
+
+    case 6:
+        std::cout << 3;
+        break;
+
+    case 7:
+        std::cout << 2;
+        break;
+
+    case 8:
+        std::cout << 2;
+        break;
+
+    case 9:
+        std::cout << 1;
+        break;
+
+    case 10:
+        std::cout << 1;
+    default:
+        std::cout << 0;
+        break;
+    }
+}
