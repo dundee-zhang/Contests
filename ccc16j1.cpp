1de66d3003cb2d0d905d96392cc957ed5288ac8f
Dundee <60833894+dundee-zhang@users.noreply.github.com>
2022-02-01 21:26:23 -0500

    PROBLEM

--- ccc16j1.cpp
+++ ccc16j1.cpp
@@ -1,5 +1,23 @@
 #include <bits/stdc++.h> 
 
 int main() {
-    
+    int w = 0;
+    char g[6];
+
+    for(int i = 0; i < 6; i++) {
+        std::cin >> g[i];
+
+        if(g[i] == 'W') {
+            w++;
+        }
+    }
+    if(w >= 5) {
+        printf("1");
+    } else if(w >= 3) {
+        printf("2");
+    } else if(w >= 1) {
+        printf("3");
+    } else {
+        printf("-1");
+    }
 }
