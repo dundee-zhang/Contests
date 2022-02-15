7907f1441440de6df965d913cffc504e39e43b16
Dundee <60833894+dundee-zhang@users.noreply.github.com>
2022-02-15 12:29:30 -0500

    Cleansed code

--- ccc14s1.cpp
+++ ccc14s1.cpp
@@ -1,32 +1,23 @@
 #include <bits/stdc++.h>
 
-int main()
-{
+int main() {
     std::vector<int> friends;
-    int friendNum;
+    int friendNum, rounds, multiplesRemoved;
 
-    scanf("%i", &friendNum);
+    scanf("%i%i", &friendNum, &rounds);
 
-    for(int i = 1; i <= friendNum; i++)
-    {
+    for(int i = 1; i <= friendNum; i++) {
         friends.push_back(i);
     }
-
-    int rounds;
-
-    scanf("%i", &rounds);
-    for(int i = 0; i < rounds; i++)
-    {
-        int multiplesRemoved;
+    
+    for(int i = 0; i < rounds; i++) {
         scanf("%i", &multiplesRemoved);
-        for(size_t q = multiplesRemoved - 1; q < friends.size(); q += multiplesRemoved - 1)
-        {
+        for(size_t q = multiplesRemoved - 1; q < friends.size(); q += multiplesRemoved - 1) {
             friends.erase(friends.begin() + q);
         }
     }
     
-    for(size_t i = 0; i < friends.size(); i++)
-    {
+    for(size_t i = 0; i < friends.size(); i++) {
         std::cout << friends.at(i) << std::endl;
     }
 }
