e97c6e7aaf70842701acf9f5fe49e65c59b4575d
Dundee <60833894+dundee-zhang@users.noreply.github.com>
2022-02-14 22:26:20 -0500

    codes

--- ccc21j4.cpp
+++ ccc21j4.cpp
@@ -4,47 +4,36 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
-    std::string b = "";
+    std::string s;
 
-    std::cin >> b;
+    std::cin >> s;
 
-    int t = b.length();
+    int l = std::count(s.begin(), s.end(), 'L');
+    int m = std::count(s.begin(), s.end(), 'M');
 
-    int l = std::count(b.begin(), b.end(), 'L');
-    int m = std::count(b.begin(), b.end(), 'M');
-
-    // COUNTING OUT OF PLACE LETTERS
-    // ints Ms and s in L section
-    int lN = 0;
+    int notL = 0;
     for(int i = 0; i < l; i++) {
-        if(b.at(i) == 'M') {
-            lN++;
-        } else if(b.at(i) == 'S') {
-            lN++;
+        if(s.at(i) != 'L') {
+            notL++;
         }
     }
-    // ints S and Ls in M section
-    int mN = 0;
+    int notM = 0;
     for(int i = l; i < l + m; i++) {
-        if(b.at(i) == 'S') {
-            mN++;
-        } else if(b.at(i) == 'L') {
-            mN++;
+        if(s.at(i) != 'M') {
+            notM++;
         }
     }
-
     int mInL = 0;
     for(int i = 0; i < l; i++) {
-        if(b.at(i) == 'M') {
-            mInL++;
+        if(s.at(i) == 'M') {
+            mInL ++;
         }
     }
     int lInM = 0;
     for(int i = l; i < l + m; i++) {
-        if(b.at(i) == 'L') {
-            lInM++;
+        if(s.at(i) == 'L') {
+            lInM ++;
         }
     }
-    std::cout << lN + mN - std::min(mInL, lInM) << std::endl;
-    
+    std::cout << notL + notM - std::min(mInL, lInM) << std::endl;
 }
