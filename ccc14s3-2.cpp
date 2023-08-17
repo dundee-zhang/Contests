fc8e0fafb5a978f302dacf52334038cdcbec7f12
Dundee <60833894+dundeezhang@users.noreply.github.com>
2023-08-17 13:13:06 -0400

    Update ccc14s3-2.cpp

--- ccc14s3-2.cpp
+++ ccc14s3-2.cpp
@@ -3,31 +3,33 @@
 
 int main() {
     std::cin.sync_with_stdio(0);
-    std::cin.tie();
+    std::cin.tie(0);
+    std::cout.tie(0);
     int n, t;
     std::cin >> t;
 
     for(int i = 0; i < t; i++) {
-        std::stack<int> lake;
-        int branch = 0;
-        std::stack<int> mt;
+        std::stack<int> lake, mt;
+        int current = 1;
+
         std::cin >> n;
         for(int temp, x = 0; x < n; x++) {
             std::cin >> temp;
             mt.push(temp);
         }
+
         while(true) {
             if(mt.empty() && lake.empty()) {
                 std::cout << "Y\n";
                 break;
             }
-            if(!mt.empty() && (int) mt.top() == branch + 1) {
+            if(!mt.empty() && (int) mt.top() == current) {
                 mt.pop();
-                branch++;
-            } else if(!lake.empty() && (int) lake.top() == branch + 1){
+                current++;
+            } else if(!lake.empty() && (int) lake.top() == current){
                 lake.pop();
-                branch++;
-            } else if(mt.empty() && lake.top() != branch + 1) {
+                current++;
+            } else if(mt.empty() && lake.top() != current) {
                 std::cout << "N\n";
                 break;
             } else {
@@ -36,5 +38,4 @@ int main() {
             }
         }
     }
-
 }
