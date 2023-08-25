a31811ff108124ad019ad9312d5eeb3736aedf33
Dundee <dundee_zhang@hotmail.com>
2023-08-25 14:48:11 -0400

    template

---
+++ --1template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vect vector
+#define iamspeed cin.sync_with_stdio(0); cin.tie(); cout.tie();
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+
+using namespace std;
+
+// variables
+
+int main() {
+    iamspeed;
+    // code start
+    
+    return 0;
+}
